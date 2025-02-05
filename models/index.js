6f95dc9598b42a933940cb3792e0b8afcb357309
Surya-0804 <surya.abothula@gmail.com>
2025-02-05 11:10:13 +0530

    chnage the db to mongo

--- models/index.js
+++ models/index.js
@@ -2,23 +2,31 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const mongoose = require('mongoose');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const MONGO_URI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/yourDatabaseName';
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.on(
+  'error',
+  console.error.bind(console, 'MongoDB connection error:')
+);
+connection.once('open', () => {
+  console.log('Connected to MongoDB successfully!');
+});
+
+// Load models dynamically
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
@@ -26,18 +34,12 @@ fs
       file.indexOf('.test.js') === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file));
+    db[model.modelName] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.mongoose = mongoose;
+db.connection = connection;
 
 module.exports = db;
