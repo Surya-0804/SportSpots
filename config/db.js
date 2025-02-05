b638e4ddd101920bcfa1bfd82f1f684771612c54
Surya-0804 <surya.abothula@gmail.com>
2025-02-05 11:53:12 +0530

    minor db issue fix

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // ✅ No need for deprecated options
+    await mongoose.connect(process.env.MONGO_URI);
+
     console.log('✅ MongoDB Atlas Connected...');
   } catch (err) {
     console.error('❌ MongoDB Connection Error:', err);
