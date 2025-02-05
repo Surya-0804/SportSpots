e81dc19cfdc0c5466afb76cc3660fd1c7143d27d
Surya-0804 <surya.abothula@gmail.com>
2025-02-05 11:20:25 +0530

    complete functionality with the stylings

--- models/event.js
+++ models/event.js
@@ -1,19 +1,42 @@
-// models/event.js
-module.exports = (sequelize, DataTypes) => {
-  const Event = sequelize.define("Event", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-  });
+const mongoose = require('mongoose');
 
-  return Event;
-};
+const EventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Event', EventSchema);
